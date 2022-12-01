0da3c3ac2c149f8e350fa5bb20ead9c410582a6d
eliasraffaelli0 <eliasraffaelli@gmail.com>
2022-12-01 11:47:35 -0300

    models created

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+//set up mongoose connection
+const mongoose = require('mongoose');
+const mongoDB = "mongodb+srv://eliasraffaelli:Volcan123@cluster0.3kbcysb.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
