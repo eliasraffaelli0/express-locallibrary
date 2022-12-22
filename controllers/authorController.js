f8e3afe505e09e0a6366efca4fdffab3ada67777
eliasraffaelli0 <eliasraffaelli@gmail.com>
2022-12-22 13:49:46 -0300

    author deatail created

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,5 +1,7 @@
 const { nextTick } = require("async");
 const Author = require("../models/author");
+const Book = require("../models/book");
+const async = require("async");
 
 // Display list of all Authors.
 exports.author_list = (req, res, next) => {
@@ -17,8 +19,32 @@ exports.author_list = (req, res, next) => {
 };
 
 // Display detail page for a specific Author.
-exports.author_detail = (req, res) => {
-    res.send(`NOT IMPLEMENTED: Author detail: ${req.params.id}`);
+exports.author_detail = (req, res, next) => {
+    async.parallel(
+        {
+            author(callback){
+                Author.findById(req.params.id).exec(callback);
+            },
+            books(callback){
+                Book.find({ author: req.params.id }, "title summary").exec(callback);
+            },
+        },
+        (err, results) => {
+            if(err){
+                return next(err);
+            }
+            if(results.author== null){
+                const err= new Error("Author not found");
+                err.status = 404;
+                return next(err);
+            }
+            res.render("author_detail", {
+                title: "Author detail",
+                author: results.author,
+                books: results.books,
+            });
+        }
+    );
 };
 
 // Display Author create form on GET.
