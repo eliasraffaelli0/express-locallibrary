e2908593716b2e0bc28b94d53c219e5941c390be
eliasraffaelli0 <eliasraffaelli@gmail.com>
2023-01-09 13:33:12 -0300

    mongodb credential stored in a env file

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const dotenv = require("dotenv");
+dotenv.config()
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -12,7 +14,8 @@ var app = express();
 
 //set up mongoose connection
 const mongoose = require('mongoose');
-const mongoDB = "mongodb+srv://eliasraffaelli:Volcan123@cluster0.3kbcysb.mongodb.net/?retryWrites=true&w=majority";
+
+const mongoDB = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.3kbcysb.mongodb.net/?retryWrites=true&w=majority`;
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
